dfb2c7bc77caabaad0f8d2cf5a36efb3f1a1aa10
Brandon <70345941@continental.edu.pe>
2021-08-13 18:53:21 -0500

    modificación a las rutas de los archivos mp3 y jpg

--- js/musicas.js
+++ js/musicas.js
@@ -3,51 +3,51 @@ const songs=[
     id: "song1",
     name: "Another one bites the dust",
     artist: "Queen",
-    url: "/assets/musicas/Another One Bites The Dust.mp3",
-    // img: "/assets/img/another one bites the dust.jpg"
-    img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+    url: "music-player/assets/musicas/Another One Bites The Dust.mp3",
+    img: "music-player/assets/img/another one bites the dust.jpg"
+    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   },
   {
     id: "song2",
     name: "Antes de los 20",
     artist: "Morat",
-    url: "/assets/musicas/Antes De Los Veinte.mp3",
-    img: "/assets/img/antes de los 20.jpg"
+    url: "music-player/assets/musicas/Antes De Los Veinte.mp3",
+    img: "music-player/assets/img/antes de los 20.jpg"
   },
   {
     id: "song3",
     name: "Dangerous",
     artist: "Michael Jackson",
-    url: "/assets/musicas/Dangerous.mp3",
-    img: "/assets/img/dangerous.jpg"
+    url: "music-player/assets/musicas/Dangerous.mp3",
+    img: "music-player/assets/img/dangerous.jpg"
   },
   {
     id: "song4",
     name: "Elements",
     artist: "Ludovico Einaudi",
-    url: "/assets/musicas/Elements.mp3",
-    img: "/assets/img/elements.jpg"
+    url: "music-player/assets/musicas/Elements.mp3",
+    img: "music-player/assets/img/elements.jpg"
   },
   {
     id: "song5",
     name: "La mujer perfecta",
     artist: "Kurt",
-    url: "/assets/musicas/La Mujer Perfecta.mp3",
-    img: "/assets/img/la mujer perfecta.jpg"
+    url: "music-player/assets/musicas/La Mujer Perfecta.mp3",
+    img: "music-player/assets/img/la mujer perfecta.jpg"
   },
   {
     id: "song6",
     name: "Todo cambio",
     artist: "Camila",
-    url: "/assets/musicas/Todo Cambio.mp3",
-    img: "/assets/img/todo cambio.jpg"
+    url: "music-player/assets/musicas/Todo Cambio.mp3",
+    img: "music-player/assets/img/todo cambio.jpg"
   },
   {
     id: "song7",
     name: "Todo de ti",
     artist: "Rauw Alejandro",
-    url: "/assets/musicas/Todo de ti.mp3",
-    img: "/assets/img/todo de ti.jpg"
+    url: "music-player/assets/musicas/Todo de ti.mp3",
+    img: "music-player/assets/img/todo de ti.jpg"
   }
 ];
 

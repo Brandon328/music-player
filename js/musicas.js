cab692418dff484a23ef54e1e6c1a70a28d4a421
Brandon <70345941@continental.edu.pe>
2021-08-13 19:13:53 -0500

    pruebita 2

--- js/musicas.js
+++ js/musicas.js
@@ -3,64 +3,64 @@ const songs=[
     id: "song1",
     name: "Another one bites the dust",
     artist: "Queen",
-    url: "/music-player/assets/musicas/Another One Bites The Dust.mp3",
-    img: "/music-player/assets/img/another one bites the dust.jpg"
-    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
-    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+    // url: "/music-player/assets/musicas/Another One Bites The Dust.mp3",
+    // img: "/music-player/assets/img/another one bites the dust.jpg"
+    url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
+    img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   },
   {
     id: "song2",
     name: "Antes de los 20",
     artist: "Morat",
-    url: "/music-player/assets/musicas/Antes De Los Veinte.mp3",
-    img: "/music-player/assets/img/antes de los 20.jpg"
-    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
-    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+    // url: "/music-player/assets/musicas/Antes De Los Veinte.mp3",
+    // img: "/music-player/assets/img/antes de los 20.jpg"
+    url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Antes%20De%20Los%20Veinte.mp3",
+    img: "https://github.com/Brandon328/music-player/blob/master/assets/img/antes%20de%20los%2020.jpg"
   },
   {
     id: "song3",
     name: "Dangerous",
     artist: "Michael Jackson",
-    url: "/music-player/assets/musicas/Dangerous.mp3",
-    img: "/music-player/assets/img/dangerous.jpg"
-    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
-    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+    // url: "/music-player/assets/musicas/Dangerous.mp3",
+    // img: "/music-player/assets/img/dangerous.jpg"
+    url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Dangerous.mp3",
+    img: "https://github.com/Brandon328/music-player/blob/master/assets/img/dangerous.jpg"
   },
   {
     id: "song4",
     name: "Elements",
     artist: "Ludovico Einaudi",
-    url: "/music-player/assets/musicas/Elements.mp3",
-    img: "/music-player/assets/img/elements.jpg"
-    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
-    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+    // url: "/music-player/assets/musicas/Elements.mp3",
+    // img: "/music-player/assets/img/elements.jpg"
+    url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Elements.mp3",
+    img: "https://github.com/Brandon328/music-player/blob/master/assets/img/elements.jpg"
   },
   {
     id: "song5",
     name: "La mujer perfecta",
     artist: "Kurt",
-    url: "/music-player/assets/musicas/La Mujer Perfecta.mp3",
-    img: "/music-player/assets/img/la mujer perfecta.jpg"
-    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
-    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+    // url: "/music-player/assets/musicas/La Mujer Perfecta.mp3",
+    // img: "/music-player/assets/img/la mujer perfecta.jpg"
+    url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/La%20Mujer%20Perfecta.mp3",
+    img: "https://github.com/Brandon328/music-player/blob/master/assets/img/la%20mujer%20perfecta.jpg"
   },
   {
     id: "song6",
     name: "Todo cambio",
     artist: "Camila",
-    url: "/music-player/assets/musicas/Todo Cambio.mp3",
-    img: "/music-player/assets/img/todo cambio.jpg"
-    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
-    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+    // url: "/music-player/assets/musicas/Todo Cambio.mp3",
+    // img: "/music-player/assets/img/todo cambio.jpg"
+    url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Todo%20Cambio.mp3",
+    img: "https://github.com/Brandon328/music-player/blob/master/assets/img/todo%20cambio.jpg"
   },
   {
     id: "song7",
     name: "Todo de ti",
     artist: "Rauw Alejandro",
-    url: "/music-player/assets/musicas/Todo de ti.mp3",
-    img: "/music-player/assets/img/todo de ti.jpg"
-    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
-    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+    // url: "/music-player/assets/musicas/Todo de ti.mp3",
+    // img: "/music-player/assets/img/todo de ti.jpg"
+    url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Todo%20de%20ti.mp3",
+    img: "https://github.com/Brandon328/music-player/blob/master/assets/img/todo%20de%20ti.jpg"
   }
 ];
 

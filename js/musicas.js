466547b5c41a3f14d842ff4fd566adb14a4504f0
Brandon <70345941@continental.edu.pe>
2021-08-13 19:09:11 -0500

    a ver ahora?

--- js/musicas.js
+++ js/musicas.js
@@ -3,8 +3,8 @@ const songs=[
     id: "song1",
     name: "Another one bites the dust",
     artist: "Queen",
-    url: "/assets/musicas/Another One Bites The Dust.mp3",
-    img: "/assets/img/another one bites the dust.jpg"
+    url: "/music-player/assets/musicas/Another One Bites The Dust.mp3",
+    img: "/music-player/assets/img/another one bites the dust.jpg"
     // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
     // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   },
@@ -12,8 +12,8 @@ const songs=[
     id: "song2",
     name: "Antes de los 20",
     artist: "Morat",
-    url: "/assets/musicas/Antes De Los Veinte.mp3",
-    img: "/assets/img/antes de los 20.jpg"
+    url: "/music-player/assets/musicas/Antes De Los Veinte.mp3",
+    img: "/music-player/assets/img/antes de los 20.jpg"
     // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
     // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   },
@@ -21,8 +21,8 @@ const songs=[
     id: "song3",
     name: "Dangerous",
     artist: "Michael Jackson",
-    url: "/assets/musicas/Dangerous.mp3",
-    img: "/assets/img/dangerous.jpg"
+    url: "/music-player/assets/musicas/Dangerous.mp3",
+    img: "/music-player/assets/img/dangerous.jpg"
     // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
     // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   },
@@ -30,8 +30,8 @@ const songs=[
     id: "song4",
     name: "Elements",
     artist: "Ludovico Einaudi",
-    url: "/assets/musicas/Elements.mp3",
-    img: "/assets/img/elements.jpg"
+    url: "/music-player/assets/musicas/Elements.mp3",
+    img: "/music-player/assets/img/elements.jpg"
     // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
     // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   },
@@ -39,8 +39,8 @@ const songs=[
     id: "song5",
     name: "La mujer perfecta",
     artist: "Kurt",
-    url: "/assets/musicas/La Mujer Perfecta.mp3",
-    img: "/assets/img/la mujer perfecta.jpg"
+    url: "/music-player/assets/musicas/La Mujer Perfecta.mp3",
+    img: "/music-player/assets/img/la mujer perfecta.jpg"
     // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
     // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   },
@@ -48,8 +48,8 @@ const songs=[
     id: "song6",
     name: "Todo cambio",
     artist: "Camila",
-    url: "/assets/musicas/Todo Cambio.mp3",
-    img: "/assets/img/todo cambio.jpg"
+    url: "/music-player/assets/musicas/Todo Cambio.mp3",
+    img: "/music-player/assets/img/todo cambio.jpg"
     // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
     // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   },
@@ -57,8 +57,8 @@ const songs=[
     id: "song7",
     name: "Todo de ti",
     artist: "Rauw Alejandro",
-    url: "/assets/musicas/Todo de ti.mp3",
-    img: "/assets/img/todo de ti.jpg"
+    url: "/music-player/assets/musicas/Todo de ti.mp3",
+    img: "/music-player/assets/img/todo de ti.jpg"
     // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
     // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
   }
